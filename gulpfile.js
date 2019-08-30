881233af4c061ed5ec640e1057dfa1013afc498b
thousand7274 <thousand7274@gmail.com>
2019-08-30 17:07:26 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+'use strict';
+//gulp
+const gulp = require('gulp');
+//sass
+const sass = require('gulp-sass');
+// media query
+const mq = require('gulp-combine-mq');
+// errror
+const plumber = require('gulp-plumber');
+// notify
+const notify = require('gulp-notify');
+// browserSync
+const browserSync = require('browser-sync');
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+  return gulp.src(['./html/sass/*.scss', './html/sass/**/*.scss'], { sourcemaps: true })
+    .pipe(sass({
+      outputStyle: 'compressed',
+    }))
+    //エラーの処理
+    .pipe(plumber({
+      errorHandler: notify.onError("Error: <%= error.message %>")
+    }))
+    //メディアクエリを一つにまとめる
+    .pipe(mq({
+      beautify: false
+    }))
+    // mapの出力
+    .pipe(gulp.dest('./html/css/', { sourcemaps: '../maps/' }))
+    .pipe(browserSync.reload({ stream: true }))
+    ;
+});
+
+// SASSのウオッチ
+gulp.task('sass:watch', function () {
+  gulp.watch(['./html/sass/*.scss', './html/sass/**/*.scss'], gulp.series('sass'));
+});
+
+
+// HTMLのウオッチ
+gulp.task('html:watch', function () {
+  gulp.watch(['./html/*.html', './html/**/*.html']).on('change', browserSync.reload);
+});
+
+//ブラウザシンク
+gulp.task('bs', function () {
+  browserSync({
+    server: {
+      baseDir: './html/',
+      index: 'index.html'
+    },
+    open: true,
+    browser: ["chrome"]
+  })
+})
+
+// gulpで動く
+gulp.task('default', gulp.parallel('html:watch', 'bs', 'sass:watch')
+);
+gulp.task('html', gulp.parallel('sass')
+);
